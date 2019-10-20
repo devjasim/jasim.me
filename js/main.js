3b10927031ad32bfd77b2e2cd605fcdcd02342e5
jasim <jasim.eng360@gmail.com>
2019-10-20 17:33:58 +0600

    jasim

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // for header 
 $(function(){
     $(window).on("scroll", function(){
-        if($(window).scrollTop() > 100){
+        if($(window).scrollTop() > 50){
             $(".navbar").addClass("active");
         }else{
             $(".navbar").removeClass("active");
