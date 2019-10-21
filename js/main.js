1662acc31898a2dd489b54336eb7c109bf8e662e
jasim <jasim.eng360@gmail.com>
2019-10-21 23:34:14 +0600

    some change

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ TxtType.prototype.tick = function() {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    this.el.innerHTML = "<span class='wrap'>I'm " +this.txt+'</span>';
 
     var that = this;
     var delta = 200 - Math.random() * 100;
