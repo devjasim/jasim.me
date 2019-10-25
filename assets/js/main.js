0b1091f282aeee242393f8579793059ad2a00d38
jasim <jasim.eng360@gmail.com>
2019-10-25 17:39:19 +0600

    jasim

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,3 +182,36 @@ $(document).ready(function () {
     });
 });
 //end progress section
+
+
+
+// font menu with jump section 
+$(document).ready(function() {
+    $('a[href*=#]').bind('click', function(e) {
+        e.preventDefault(); // prevent hard jump, the default behavior
+
+        var target = $(this).attr("href"); // Set the target as variable
+
+        // perform animated scrolling by getting top-position of target-element and set it as scroll target
+        $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+        }, 600, function() {
+                location.hash = target; //attach the hash (#jumptarget) to the pageurl
+        });
+
+        return false;
+    });
+});
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+    // Assign active class to nav links while scolling
+    $('.jump').each(function(i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('.nav-item a.active').removeClass('active');
+            $('.nav-item a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
+// font menu with jump section 
