bf2e347a0fee5eacc7ffbb9aae33f3c65b57eeff
jasim <jasim.eng360@gmail.com>
2019-11-04 21:38:23 +0600

    jasim

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,21 +185,22 @@ $(document).ready(function () {
 
 
 
-// font menu with jump section 
+
+
 $(document).ready(function() {
     $('a[href*=#]').bind('click', function(e) {
-        e.preventDefault(); // prevent hard jump, the default behavior
+            e.preventDefault(); // prevent hard jump, the default behavior
 
-        var target = $(this).attr("href"); // Set the target as variable
+            var target = $(this).attr("href"); // Set the target as variable
 
-        // perform animated scrolling by getting top-position of target-element and set it as scroll target
-        $('html, body').stop().animate({
-                scrollTop: $(target).offset().top
-        }, 600, function() {
-                location.hash = target; //attach the hash (#jumptarget) to the pageurl
-        });
+            // perform animated scrolling by getting top-position of target-element and set it as scroll target
+            $('html, body').stop().animate({
+                    scrollTop: $(target).offset().top
+            }, 1000, function() {
+                    location.hash = target; //attach the hash (#jumptarget) to the pageurl
+            });
 
-        return false;
+            return false;
     });
 });
 
@@ -208,10 +209,9 @@ $(window).scroll(function() {
 
     // Assign active class to nav links while scolling
     $('.jump').each(function(i) {
-        if ($(this).position().top <= scrollDistance) {
-            $('.nav-item a.active').removeClass('active');
-            $('.nav-item a').eq(i).addClass('active');
-        }
+            if ($(this).position().top <= scrollDistance) {
+                    $('.navbar-nav a.active').removeClass('active');
+                    $('.navbar-nav a').eq(i).addClass('active');
+            }
     });
 }).scroll();
-// font menu with jump section 
