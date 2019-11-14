2f877e7bc7affbf6432cfe0702c2000ce928c8d7
jasim <jasim.eng360@gmail.com>
2019-11-14 15:42:46 +0600

    jasim

--- assets/js/main.js
+++ assets/js/main.js
@@ -222,3 +222,19 @@ $(window).scroll(function() {
             }
     });
 }).scroll();
+
+
+$(document).ready(function(){
+	var $grid = $('.grid-group').isotope({
+		itemSelector: '.grid',
+		masonry: {
+		  columnWidth:1,
+		}
+	  });
+	
+	  // filter items on button click
+	  $('.filter-button').on( 'click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({ filter: filterValue });
+	});
+})
